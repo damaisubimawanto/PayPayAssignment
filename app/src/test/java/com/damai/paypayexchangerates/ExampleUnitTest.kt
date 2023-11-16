841526a8fa872a6004dda31814b6c09eca4b568f
Damai Subimawanto <damai.subimawanto@gmail.com>
2023-11-16 18:47:50 +0700

    Moved compress unit tests into ExampleUnitTest.kt.

--- app/src/test/java/com/damai/paypayexchangerates/ExampleUnitTest.kt
+++ app/src/test/java/com/damai/paypayexchangerates/ExampleUnitTest.kt
@@ -10,8 +10,64 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    // input : "aaaabbcccab"
+    // output : "a4b2c3a1b1"
+    private fun compress(input: String): String {
+        var number = 0
+        var currentChar = ""
+        val stringBuilder = StringBuilder()
+        val lastTextPosition = input.length - 1
+        for (index in input.indices) {
+            val character = input.substring(index, index + 1)
+            when (currentChar) {
+                "" -> {
+                    currentChar = character
+                    number++
+                }
+                character -> number++
+                else -> {
+                    stringBuilder.append("$currentChar$number")
+                    currentChar = character
+                    number = 1
+                }
+            }
+            if (index == lastTextPosition) {
+                stringBuilder.append("$currentChar$number")
+            }
+        }
+        return stringBuilder.toString()
+    }
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun compressUnitTest() {
+        val input = "aaaabbcccab"
+        val expected = "a4b2c3a1b1"
+        val actual = compress(input)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun compress2UnitTest() {
+        val input = "aaaabbcccaaa"
+        val expected = "a4b2c3a3"
+        val actual = compress(input)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun compress3UnitTest() {
+        val input = "aaaabbcccaaabbbbbb"
+        val expected = "a4b2c3a3b6"
+        val actual = compress(input)
+
+        assertEquals(expected, actual)
     }
 }
